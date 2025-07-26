8515c753e171ecba8fcfcc61557366298e15a166
Shubham Bhardwaj <apple@Apples-MacBook-Pro.local>
2025-07-26 19:06:14 +0530

    initial spring boot web boiler plate

---
+++ src/test/java/com/shubh/springbootweb1/Springbootweb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubh.springbootweb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootweb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
