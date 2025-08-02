4a4e14e62e735683aeb1a4678ec5d5f40602d856
Shubham Bhardwaj <apple@Apples-MacBook-Pro.local>
2025-08-02 18:18:15 +0530

    request mapping for spring

--- src/main/java/com/shubh/springbootweb1/HomeController.java
+++ src/main/java/com/shubh/springbootweb1/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -14,17 +15,28 @@ public class HomeController {
         return "index.jsp";
     }
 
+    // servelt way of handling requests
+//    @RequestMapping("add")
+//    public String add(HttpServletRequest req, HttpSession session) {
+//        req.getParameter("num1");
+//        req.getParameter("num2");
+//        int n1 = Integer.parseInt(req.getParameter("num1"));
+//        int n2 = Integer.parseInt(req.getParameter("num2"));
+//        int result = n1 + n2;
+//        // Storing the result in the session
+//        session.setAttribute("result", result);
+//        // Logging the result to the console
+//        System.out.println("HomeController.add() called");
+//        return "result.jsp";
+//    }
+    // Spring MVC way of handling requests
     @RequestMapping("add")
-    public String add(HttpServletRequest req, HttpSession session) {
-        req.getParameter("num1");
-        req.getParameter("num2");
-        int n1 = Integer.parseInt(req.getParameter("num1"));
-        int n2 = Integer.parseInt(req.getParameter("num2"));
-        int result = n1 + n2;
+    public String add(@RequestParam("num1") int num, int num2, HttpSession session) {
+        int result = num + num2;
         // Storing the result in the session
         session.setAttribute("result", result);
         // Logging the result to the console
-        System.out.println("HomeController.add() called");
+        System.out.println("HomeController.add() called with num1: " + num + ", num2: " + num2 + ", result: " + result);
         return "result.jsp";
     }
 }
