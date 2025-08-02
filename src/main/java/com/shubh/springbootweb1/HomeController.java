fd19a290c38dcc120c8d699abec5528e88599ab2
Shubham Bhardwaj <apple@Apples-MacBook-Pro.local>
2025-08-02 19:13:29 +0530

    application properties for prefix and postfix for views

--- src/main/java/com/shubh/springbootweb1/HomeController.java
+++ src/main/java/com/shubh/springbootweb1/HomeController.java
@@ -3,6 +3,7 @@ package com.shubh.springbootweb1;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,7 +13,7 @@ public class HomeController {
     @RequestMapping("/")
     public String home() {
         System.out.println("HomeController.home() called");
-        return "index.jsp";
+        return "index";
     }
 
     // servelt way of handling requests
@@ -30,13 +31,22 @@ public class HomeController {
 //        return "result.jsp";
 //    }
     // Spring MVC way of handling requests
+//    @RequestMapping("add")
+//    public String add(@RequestParam("num1") int num, int num2, HttpSession session) {
+//        int result = num + num2;
+//        // Storing the result in the session
+//        session.setAttribute("result", result);
+//        // Logging the result to the console
+//        System.out.println("HomeController.add() called with num1: " + num + ", num2: " + num2 + ", result: " + result);
+//        return "result.jsp";
+//    }
+
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num, int num2, HttpSession session) {
+    public String add(@RequestParam("num1") int num, int num2, Model model) {
         int result = num + num2;
-        // Storing the result in the session
-        session.setAttribute("result", result);
-        // Logging the result to the console
+        // Adding the result to the model
+        model.addAttribute("result", result);
         System.out.println("HomeController.add() called with num1: " + num + ", num2: " + num2 + ", result: " + result);
-        return "result.jsp";
+        return "result";
     }
 }
